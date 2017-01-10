538803f4392ce7667ea7edadfd3ddafeec108ecb
Benjamin ETIENNE <benjamin.etienne@centraliens-lille.org>
2017-01-10 17:17:57 +0100

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -4,62 +4,54 @@ import threading
 
 class ThreadReception(threading.Thread):
     """
-	Receiving thread
+    Receiving thread
     Params:
 	    cl : client socket
-	"""
-	def __init__(self, cl):
-		threading.Thread.__init__(self)
-		self.cl = cl
+    """
+    def __init__(self, cl):
+	threading.Thread.__init__(self)
+	self.cl = cl
 
-	def run(self):
-		nom = self.getName()
+    def run(self):
+	nom = self.getName()
 
-		while True:
-			msg_recu = self.cl.recv(1024).decode()
+	while True:
+	    msg_recu = self.cl.recv(1024).decode()
 
-			if "EXIT" in msg_recu:
-				break
+	    if "EXIT" in msg_recu:
+	        break
 
-			print("{}".format(msg_recu))
-			msg_envoi = msg_recu.encode()
+	    print("{}".format(msg_recu))
+	    msg_envoi = msg_recu.encode()
 
-			for name in clients:
-				if name != nom:					
-					clients[name].send(msg_envoi)
+	    for name in clients:
+	    	if name != nom:					
+	    	clients[name].send(msg_envoi)
 
-		self.cl.close()
-		del clients[name]
-		print("{} left the conversation".format(name))
+	self.cl.close()
+	del clients[name]
+	print("{} left the conversation".format(name))
 
 
 if __name__ == '__main__':
-	clients = {}
-	rep = ''
+    clients = {}
+    rep = ''
 
-	s = socket.socket()
-	host = socket.gethostname()
-	port = 12345
-	s.bind((host, port))
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 12345
+    s.bind((host, port))
 
-	s.listen(5)
+    s.listen(5)
 
 
-	print("Starting Server...")
+    print("Starting Server...")
 
-	while True:
-		c, addr = s.accept()
-		print("Connection from {:}".format(addr))
-		th = ThreadReception(c)
-		th.start()
-		name = th.getName()
-		clients[name] = c
-		# c.send("Hello ! What is your name ?".encode())
-		# data = c.recv(1024).decode()	
-		# if data:	
-		# 	print("Client says :: {:}".format(data))
-		# 	c.send(data.encode())
-		# else:
-		# 	break	
+    while True:
+	c, addr = s.accept()
+	print("Connection from {:}".format(addr))
+	th = ThreadReception(c)
+	th.start()
+	name = th.getName()
+	clients[name] = c
 
-	# c.close()
